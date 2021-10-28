454a5c7f0e01b7d4a1996bf7a4c4932326c47976
Nitesh <niteshp27@gmail.com>
2021-10-28 21:10:51 +0530

    updated html

--- src/App.js
+++ src/App.js
@@ -19,10 +19,7 @@ function App() {
     <Router>
     <div>
       <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+
       </Helmet>
 
       <nav className="bg-gray-800">
@@ -91,7 +88,7 @@ function App() {
             <div className="max-w-8xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
               <div className="flex items-center justify-between flex-wrap">
                 <div className="w-0 flex-1 flex items-center">
-                  <span className="flex p-2 rounded-lg bg-gray-800">
+                  <span className="flex p-4 rounded-lg bg-gray-800">
                     {/*<!-- Heroicon name: outline/speakerphone -->*/}
                     <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
--- src/component/Exploreenjoy.js
+++ src/component/Exploreenjoy.js
@@ -25,7 +25,7 @@ const Exploreenjoy = () => {
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h1 className="text-3xl font-bold text-gray-900">
-                    <span className="text-prm font-extrabold">Happiness</span> <span className="text-sec"> Crate</span>
+                    <span className="text-prm font-extrabold leading-normal">Happiness</span> <span className="text-sec"> Crate</span>
                 </h1>
                 </div>
             </header>
